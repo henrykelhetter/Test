f6f296361cf7b700dd92317b8892819078be97a0
Henry <kelhetter@gmail.com>
2016-11-30 14:00:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+git hub project
+max paine
+total 151
+drwxr-xr-x 1 AMADEUS 197121      0 nov.  30 12:10 ./
+drwxr-xr-x 1 AMADEUS 197121      0 nov.  23 08:49 ../
+drwxr-xr-x 1 AMADEUS 197121      0 nov.  30 12:03 .git/
+-rw-r--r-- 1 AMADEUS 197121    627 nov.  21 14:30 index.html
+-rw-r--r-- 1 AMADEUS 197121   1096 nov.  21 14:29 main.js
+-rw-r--r-- 1 AMADEUS 197121      0 nov.  21 10:51 Nouveau document texte.txt
+drwxr-xr-x 1 AMADEUS 197121      0 nov.  21 13:58 Photo/
+-rw-r--r-- 1 AMADEUS 197121 124624 nov.  11 09:39 Photo.zip
+-rw-r--r-- 1 AMADEUS 197121     26 nov.  30 12:11 readme.md
+-rw-r--r-- 1 AMADEUS 197121    168 nov.  13 18:31 style.css
+-rw-r--r-- 1 AMADEUS 197121     15 nov.  21 10:51 text.txt
---
+++ main.js
@@ -0,0 +1,60 @@
+var jcontent1 = {
+  "firstName": "MAX",
+  "lastName": "Smith",
+  "isAlive": true,
+  "age": 25,
+  "address": {
+    "streetAddress": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postalCode": "10021-3100"
+  },
+  "phoneNumbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    },
+    {
+      "type": "mobile",
+      "number": "123 456-7890"
+    }
+  ],
+  "children": [],
+  "spouse": null
+}
+
+
+var jcontent2 = {
+  "firstName": "John",
+  "lastName": "Rambo",
+  "isAlive": true,
+  "age": 25,
+  "address": {
+    "streetAddress": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postalCode": "10021-3100"
+  },
+  "phoneNumbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    },
+    {
+      "type": "mobile",
+      "number": "123 456-7890"
+    }
+  ],
+  "children": [],
+  "spouse": null
+}
+
+var users = {user1 : jcontent1, user2 : jcontent2};
